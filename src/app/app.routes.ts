9056acd4dbf161f47e93830e73129b2e13eda73f
thewebalok <alokprakash.net@gmail.com>
2025-08-17 16:01:31 +0530

    Update routes and fix hash routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { DashboardComponent } from './admin/manage-categories/dashboard/dashboard.component';
 import { AddCategoryComponent } from './admin/manage-categories/add-category/add-category.component';
 import { CertificatesComponent } from './admin/manage-categories/certificates/certificates.component';
 
-
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', component: PortfolioComponent },
-   { path: 'dashboard', component: DashboardComponent },
-   { path: 'addCategory', component: AddCategoryComponent },
-   { path: 'addCertificates', component: CertificatesComponent},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'addCategory', component: AddCategoryComponent },
+  { path: 'addCertificates', component: CertificatesComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
